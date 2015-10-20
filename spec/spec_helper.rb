05d620ff29f136fda70278a2d4cd986995754e9b
root <mrniceguy4net@yahoo.com>
2015-10-20 15:43:53 +0100

    tests improved and done

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
-#require 'coveralls'
-#Coveralls.wear!
+require 'coveralls'
+Coveralls.wear!
+#require 'simplecov'
+#SimpleCov.start
 
 require "rspec"
 require_relative '../lib/engine'
 require_relative '../lib/fileop'
 require_relative '../lib/hangman'
 require_relative '../lib/game'
-require 'simplecov'
-SimpleCov.start
+#require 'simplecov'
+#SimpleCov.start
